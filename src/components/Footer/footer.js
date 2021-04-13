587a9d5bce7a36bac6774ff9e142bc4dc1f74275
Azazel5 <subhanga2013@gmail.com>
2021-04-12 22:45:48 -0500

    Polished up the footer with svgs and flexbox

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,14 @@
 import './footer.scss'
 
+import Sprite from '../../assets/SVG/symbol-defs.svg'
+
 const Footer = props => {
     return (
         <footer className="footer">
             <div className="footer__top">
                 <div className="footer__top-child">
                     <h2>About</h2>
-                    <hr class="solid"></hr>
+                    <hr className="solid"></hr>
                     <a href="$#">Mission</a>
                     <a href="$#">History</a>
                     <a href="$#">Impact</a>
@@ -17,13 +19,45 @@ const Footer = props => {
                 </div>
                 <div className="footer__top-child">
                     <h2>News</h2>
-                    <hr class="solid"></hr>
+                    <hr className="solid"></hr>
                     <a href="$#">Stories</a>
                     <a href="$#">Social Media</a>
                     <a href="$#">Media Releases</a>
                     <a href="$#">Share your story</a>
                 </div>
-                <div className="footer__top-child">Contact</div>
+                <div className="footer__top-child">
+                    <h2>Contact</h2>
+                    <hr className="solid"></hr>
+                    <div className="social-icon-container">
+                        <a href="$#">
+                            <svg className="social-icon-container__svg social-icon-container__svg--social">
+                                <use xlinkHref={`${Sprite}#icon-facebook`}></use>
+                            </svg>
+                        </a>
+
+                        <a href="$#">
+                            <svg className="social-icon-container__svg social-icon-container__svg--social">
+                                <use xlinkHref={`${Sprite}#icon-instagram`}></use>
+                            </svg>
+                        </a>
+
+                        <a href="$#">
+                            <svg className="social-icon-container__svg social-icon-container__svg--social">
+                                <use xlinkHref={`${Sprite}#icon-twitter`}></use>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div className="contact-phone-container">
+                        <svg className="social-icon-container__svg social-icon-container__svg--nonsocial"><use xlinkHref={`${Sprite}#icon-compass2`}></use></svg>
+                        <p>1234 Lazimpat Street, Maharajgunj 55415, Nepal</p>
+                    </div>
+
+                    <div className="contact-phone-container">
+                        <svg className="social-icon-container__svg social-icon-container__svg--nonsocial"><use xlinkHref={`${Sprite}#icon-mobile2`}></use></svg>
+                        <p>+977 9810203040, +977 9879124210</p>
+                    </div>
+                </div>
             </div>
 
             <div className="footer__bottom">
