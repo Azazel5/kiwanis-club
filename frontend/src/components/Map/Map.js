d98b41f52dabc9b3505e2e592bb433e86fb1a410
Azazel5 <subhanga2013@gmail.com>
2021-06-21 17:59:58 -0400

    Website made fully responsive from 1200px down to 320px

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,10 +1,11 @@
+import './Map.scss'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Map = props => {
     const { latitude, longitude, zoom, pinText } = props
 
     return (
-        <MapContainer center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true} style={{height: '60rem'}}>
+        <MapContainer className="map-container" center={[latitude, longitude]} zoom={zoom} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
