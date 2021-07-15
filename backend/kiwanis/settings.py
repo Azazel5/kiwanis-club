a85ce5ef56df1c9ce97b7ba17ebe04e054240357
Azazel5 <subhanga2013@gmail.com>
2021-07-15 16:11:13 -0500

    x

--- backend/kiwanis/settings.py
+++ backend/kiwanis/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env('KIWANIS_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kiwanisnepali.org', 'backend-prod.eba-yutmxpkf.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*kiwanisnepali.org', 'kiwanisnepali.org',
+                 'backend-prod.eba-yutmxpkf.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
