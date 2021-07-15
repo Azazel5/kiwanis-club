6a28cb22b594d786581fdab7923111928467e622
Azazel5 <subhanga2013@gmail.com>
2021-07-14 21:12:11 -0500

    x

--- backend/kiwanis/settings.py
+++ backend/kiwanis/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('KIWANIS_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://test22.ap-south-1.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['backend-prod.eba-yutmxpkf.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
