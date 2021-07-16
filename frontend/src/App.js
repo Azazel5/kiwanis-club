b30d1ca3c0d4d2db6b22293b46e08ac5af08ab21
Azazel5 <subhanga2013@gmail.com>
2021-07-16 09:32:04 -0500

    x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 import './App.scss'
 import Navigation from './components/navigation/navigation'
+import headerPhoto from './assets/Images/header-photo.jpeg'
 import MenuOverlay from './components/MenuOverlay/MenuOverlay'
 import NewsGallery from './components/NewsSection/NewsGallery/NewsGallery'
 import NewsStory from './components/NewsSection/NewsStory/NewsStory'
@@ -80,7 +81,7 @@ const App = () => {
                     <div className="app">
 
                         <div className="app__image-container">
-                            <img src={`${process.env.REACT_APP_AWS_MEDIA_DIRNAME}header-photo.051909b9.jpeg`} alt="header" className="app__image-container__image" />
+                            <img src={headerPhoto} alt="header" className="app__image-container__image" />
                         </div>
 
                         <div className="app__purpose-container">
