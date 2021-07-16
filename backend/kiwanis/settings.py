bda5ddab02b28a535d9000c9d1cbd1151c41b3ae
Azazel5 <subhanga2013@gmail.com>
2021-07-15 23:22:38 -0500

    Finished

--- backend/kiwanis/settings.py
+++ backend/kiwanis/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_DIRS = [
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build', 'static'),
 ]
 
