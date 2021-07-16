70a28f06b831d41e649183193a03392b070ea4bb
Azazel5 <subhanga2013@gmail.com>
2021-07-15 21:55:49 -0500

    Finished

--- backend/kiwanis/storage_backends.py
+++ backend/kiwanis/storage_backends.py
@@ -4,9 +4,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
+    file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
-    file_overwrite = False
+    file_overwrite = True
