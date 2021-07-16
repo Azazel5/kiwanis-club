e2ef6c5e79325e0324167d2fe597d7fe769188cc
Azazel5 <subhanga2013@gmail.com>
2021-07-15 22:34:54 -0500

    Finished

--- backend/kiwanis/settings.py
+++ backend/kiwanis/settings.py
@@ -215,7 +215,7 @@ AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'kiwanis.custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'kiwanis.custom_storages.MediaStorage'
